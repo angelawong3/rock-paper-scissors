cb3c1766cc0aea91135d1f3dd2c1d687b057ac2d
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-04-21 21:04:33 +0100

    save changes

---
+++ script.js
@@ -0,0 +1,30 @@
+// store wins, ties, and losses
+var wins = 0;
+var ties = 0;
+var losses = 0;
+var options = ["R", "P", "S"];
+
+function main(){
+    // take user input
+    const userOption = window.prompt("Please pick an option") ?? "";
+    const captitalizeduserOption = userOption.toUpperCase();
+
+
+// computer picks a choice at random
+const random = Math.floor(Math.random()*options.length)
+const compOption = options[random];
+
+// compute who won
+if (userOption === compOption) {
+    window.alert("It's a tie!");
+    ties++;
+}
+
+}
+
+
+
+// play again option
+
+
+
